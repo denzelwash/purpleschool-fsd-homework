a3759fcc905e48d8e0901a809c3ce51b01fba26b
Denis Obolevich <denzelwash33@yandex.ru>
2025-08-18 20:36:22 +0300

    feat: добавил shared ui

--- src/app/layouts/RootLayout.tsx
+++ src/app/layouts/RootLayout.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router'
+import { Footer } from '../../shared'
 
 export const RootLayout = () => {
 	return (
 		<div>
 			<div>Шапка</div>
 			<Outlet />
-			<div>Футер</div>
+			<Footer />
 		</div>
 	)
 }
