817a4dbadb1caf4401e4caf3a38890aa74136fcc
Denis Obolevich <denzelwash33@yandex.ru>
2025-08-19 21:26:38 +0300

    feat: вывел навигацию

--- src/app/layouts/RootLayout/RootLayout.tsx
+++ src/app/layouts/RootLayout/RootLayout.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from 'react-router'
 import { Sidebar } from '../../../widgets/sidebar'
+import styles from './RootLayout.module.css'
 
 export const RootLayout = () => {
 	return (
-		<div>
-			<Outlet />
+		<div className={styles['root-layout']}>
 			<Sidebar />
+			<div className={styles.main}>
+				<Outlet />
+			</div>
 		</div>
 	)
 }
--- src/widgets/sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,9 +1,49 @@
+import { NavLink } from 'react-router'
+import styles from './Sidebar.module.css'
+import { ROUTE_PATH } from '../../../../shared/config'
+import classNames from 'classnames'
+
 export const Sidebar = () => {
+	const nav = [
+		{
+			url: ROUTE_PATH.COURSES.BASE,
+			name: 'Курсы'
+		},
+		{
+			url: ROUTE_PATH.SKILL.BASE,
+			name: 'Карта развития'
+		},
+		{
+			url: ROUTE_PATH.CLUB,
+			name: 'PurpleПлюс'
+		},
+		{
+			url: ROUTE_PATH.REFERRAL,
+			name: 'Бонусы'
+		},
+		{
+			url: ROUTE_PATH.PROFILE.BASE,
+			name: 'Профиль'
+		},
+		{
+			url: ROUTE_PATH.COMMUNITY,
+			name: 'Сообщество'
+		},
+		{
+			url: ROUTE_PATH.HELP,
+			name: 'Помощь'
+		}
+	]
+
 	return (
-		<>
-			<div>Sidebar Item</div>
-			<div>Sidebar Item</div>
-			<div>Sidebar Item</div>
-		</>
+		<nav className={styles.sidebar}>
+			{nav.map((item) => {
+				return (
+					<NavLink className={({ isActive }) => classNames('link', { active: isActive })} to={item.url} key={item.url}>
+						{item.name}
+					</NavLink>
+				)
+			})}
+		</nav>
 	)
 }
