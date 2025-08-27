3d0ed37d709a0ba33a1c191be9b2d69aad67eacb
Denis Obolevich <denzelwash33@yandex.ru>
2025-08-27 18:10:25 +0300

    refactor: сделал shared Card и Graph более простыми компонентами

--- src/shared/ui/Card/Card.tsx
+++ src/shared/ui/Card/Card.tsx
@@ -1,18 +1,6 @@
 import style from './Card.module.css'
 import type { CardProps } from './Card.props'
 
-export const Card = ({ image, title, tags, content, footer }: CardProps) => {
-	return (
-		<div className={style.card}>
-			<div className={style.image}>
-				<img src={image} />
-			</div>
-			<div className={style.content}>
-				<h2>{title}</h2>
-				{content}
-			</div>
-			<div className={style.tags}>{tags}</div>
-			<div className={style.footer}>{footer}</div>
-		</div>
-	)
+export const Card = ({ children }: CardProps) => {
+	return <div className={style.card}>{children}</div>
 }
--- src/shared/ui/Graph/Graph.tsx
+++ src/shared/ui/Graph/Graph.tsx
@@ -1,5 +1,11 @@
 import styles from './Graph.module.css'
+import type { GraphProps } from './Graph.props'
 
-export const Graph = () => {
-	return <div className={styles.graph}>График</div>
+export const Graph = ({ title, children }: GraphProps) => {
+	return (
+		<div className={styles.graph}>
+			{title && <h2 className={styles.title}>{title}</h2>}
+			<div>{children}</div>
+		</div>
+	)
 }
